d8d62f4ab2005aad54d7bca75d4b0ff0d6724edc
Rachita Pant <rachitap89@gmail.com>
2025-04-12 19:22:07 +0530

    env fixed

--- src/supabase/supabaseClient.ts
+++ src/supabase/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://zbpqoypuxxngicligpky.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpicHFveXB1eHhuZ2ljbGlncGt5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyNzczMjMsImV4cCI6MjA1OTg1MzMyM30.FGnGlGwmC5cFQ9Kx7o6LUI7pTRDuD0l7UV15n1zzljU';
-
+const supabaseUrl =   process.env.NEXT_PUBLIC_SUPABASE_URL || ''
+  
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
