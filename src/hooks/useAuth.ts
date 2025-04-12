fc6e5641dcdb3f930a93d77bf94b5b4111ad5337
Rachita Pant <rachitap89@gmail.com>
2025-04-12 17:11:08 +0530

    fix 6

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react';
 import { supabase } from '@/supabase/supabaseClient';
+import { User } from '@supabase/supabase-js';  
 
 export const useAuth = () => {
-  const [user, setUser] = useState<any>(null);
-
+  const [user, setUser] = useState<User | null>(null);  
   useEffect(() => {
     const getUser = async () => {
       const { data: { user } } = await supabase.auth.getUser();
