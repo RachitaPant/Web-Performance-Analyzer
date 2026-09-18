f5314534e9d20aa74863d3c8daa8ac511e8d2e7c
Rachita Pant <rachitap89@gmail.com>
2026-09-19 00:01:13 +0530

    f2

---
+++ jest.setup.js
@@ -0,0 +1,3 @@
+// Learn about this file: https://github.com/vercel/next.js/blob/canary/examples/with-jest/jest.setup.js
+// Applying JSDOM testing environment to test component that need DOM
+import '@testing-library/jest-dom'
--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  experimental: {
-    turbopack: false,
-  },
   webpack: (config, { isServer }) => {
     if (isServer) {
       config.externals = [...(config.externals || []), "lighthouse", "chrome-launcher"];
